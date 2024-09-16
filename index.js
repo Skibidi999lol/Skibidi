1247a861d96327a3b0b38dc434b9570361df84ef
Skibidi999lol <philipbrzykcy@gmail.com>
2024-09-16 18:05:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.listen(port, () => {
 });
 
 const statusMessages = ["Breathing"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusTypes = [ 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
